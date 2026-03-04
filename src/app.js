fc956fd5f8a06d548c0a761f6483333c48e53fe7
Anh Baor <bachiazed2k4dn@gmail.com>
2026-03-04 19:35:44 +0700

    feat(categorty): finish

--- src/app.js
+++ src/app.js
@@ -1,21 +1,25 @@
 const express = require('express');
+const morgan = require('morgan');
+
 //import routes
 const authRoutes = require('./routes/auth.routes');
 const uploadRoutes = require('./routes/upload.routes');
+const categoryRoutes = require('./routes/category.routes');
 //middleware for handling errors
 
 const errorHandler = require('./middlewares/errorHandler');
 
-
 const app = express();
 
 app.use(express.json());
+app.use(morgan('dev'));
 
 app.use('/api/auth', authRoutes);
 
 app.use('/api/uploads', uploadRoutes);
 
-app.use(errorHandler);
+app.use('/api/categories', categoryRoutes);
 
+app.use(errorHandler);
 
 module.exports = app;
