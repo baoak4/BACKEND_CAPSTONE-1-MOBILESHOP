3565a9b6cc012b65c3dee7818595da2bd0715ec0
Anh Baor <bachiazed2k4dn@gmail.com>
2026-05-03 15:52:26 +0700

    feat(upload): add single image upload API test

--- tests/upload.test.js
+++ tests/upload.test.js
@@ -23,3 +23,23 @@ async function uploadImage() {
         console.error("UPLOAD ERROR:", err.response?.data || err.message);
     }
 }
+
+// ===== UPLOAD MULTIPLE IMAGES =====
+async function uploadMultiple() {
+    try {
+        const form = new FormData();
+
+        form.append("files", fs.createReadStream("./img1.jpg"));
+        form.append("files", fs.createReadStream("./img2.jpg"));
+
+        const res = await axios.post(`${BASE_URL}/multiple`, form, {
+            headers: {
+                ...form.getHeaders()
+            }
+        });
+
+        console.log("UPLOAD MULTIPLE:", res.data);
+    } catch (err) {
+        console.error("UPLOAD MULTI ERROR:", err.response?.data || err.message);
+    }
+}
